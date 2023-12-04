d93d1e53f1224745fc4675476f2174efb247ebcb
pabloscyzoryk <pawel.cyrzyk@gmail.com>
2023-12-04 20:20:41 +0100

    finishing touches, added password protected version

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,9 @@
 // CLIENT for Octopus 4.0.1-alpha
 // imports
+import clearLastLines from './utils/clearLastLines';
 import writeCommand from './utils/writeCommand';
 import banner from './commands/banner';
+import { rl } from './utils/rl';
 
 // globals
 global.isLoading = false;
@@ -12,9 +14,36 @@ global.nick = 'guest';
 global.basicError = 'You have and error in your octopus syntax. Type "help" for more information.';
 
 // app
-(async () => {
+const main = async () => {
   await banner.execute();
-  console.log(`Welcome to the octopus ${global.serialNumber}!`);
-  console.log('Type "help" to see all available commands.');
+  console.log(`Welcome to the octopus ${global.serialNumber}!
+  Type "help" to see all available commands.
+  `);
   writeCommand();
+};
+
+// password protected version
+
+// NEVER change this:
+let input = '';
+
+// MAYBE change this (not recommended):
+let prompt = '';
+
+// change this (OPTIONAL, "szczur" is nice):
+let password = 'szczur'; // <- password example
+
+(async () => {
+  do {
+    input = await new Promise(resolve => {
+      rl.question(prompt, resolve);
+    });
+    clearLastLines(1);
+  } while (input != password);
+
+  console.clear();
+  main();
 })();
+
+// unprotected version
+// main();
