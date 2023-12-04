c109f2d7846a887a9eabc9ba32c7bfa6a3d4bafd
pabloscyzoryk <pawel.cyrzyk@gmail.com>
2023-12-04 19:08:29 +0100

    fixed prefix command

--- client/src/commands/prefix.ts
+++ client/src/commands/prefix.ts
@@ -1,17 +1,17 @@
 // imports
-import type Command from "../types/command";
+import type Command from '../types/command';
 
 const execute = (input: string) => {
-    input === '&nbsp' ? global.prefix = ' ' : global.prefix = input;
-    console.log(`Command prefix set to ${input}`);
-}
+  global.prefix = input.replace(/&nbsp/g, ' ');
+  console.log(`Command prefix set to ${input}`);
+};
 
 const prefix: Command = {
-    names: ["prefix"],
-    description: 'Usage: "prefix" <prefix> ; sets the command prefix to <prefix>, can use &nbsp for space',
-    syntax: new RegExp(/^prefix\s.+/),
-    usage: '"prefix" <prefix>',
-    execute,
-}
+  names: ['prefix'],
+  description: 'Usage: "prefix" <prefix> ; sets the command prefix to <prefix>, can use &nbsp for space',
+  syntax: new RegExp(/^prefix\s.+/),
+  usage: '"prefix" <prefix>',
+  execute,
+};
 
 export default prefix;
