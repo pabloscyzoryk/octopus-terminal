492a2c00e5a833f56b6490aace406772d4da56ac
pabloscyzoryk <pawel.cyrzyk@gmail.com>
2023-12-05 00:07:30 +0100

    add condition for baked env in port command

--- client/src/commands/port.ts
+++ client/src/commands/port.ts
@@ -1,10 +1,15 @@
 // imports
+import envSetupForCompilation from '../utils/envSetupForCompilation';
 import Command from '../types/command';
 import findConfig from 'find-config';
 import * as dotenv from 'dotenv';
 
 // env init
-dotenv.config({ path: findConfig('.env') });
+if (envSetupForCompilation.PORT) {
+  process.env.PORT = envSetupForCompilation.PORT;
+} else {
+  dotenv.config({ path: findConfig('.env') });
+};
 
 const execute = () => {
   console.log('PORT: ' + process.env.PORT || 'http://localhost:8000');
@@ -12,7 +17,7 @@ const execute = () => {
 
 const port: Command = {
   names: ['port'],
-  description: 'Usage: "port" ; Shows the port of the server',
+  description: 'Usage: "port" ; Shows the port of the connected server',
   usage: '"port"',
   syntax: new RegExp(/^(port)$/),
   execute,
